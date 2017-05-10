aa9fa478bc86ed7ab50abc59de83f98259624b62
Wil Thomason <wil.thomason@gmail.com>
2017-05-10 16:18:01 -0400

    Fix stupid module reference bug

--- rplugin/python3/buildit/builders/__init__.py
+++ rplugin/python3/buildit/builders/__init__.py
@@ -1,11 +1,11 @@
 ''' Standard builder definitions for BuildIt '''
 
-import buildit.builders.cargo as cargo
-import buildit.builders.cmake as cmake
-import buildit.builders.gobuild as gobuild
-import buildit.builders.make as make
-import buildit.builders.oasis as oasis
-import buildit.builders.stack as stack
+from . import cargo
+from . import cmake
+from . import gobuild
+from . import make
+from . import oasis
+from . import stack
 
 BUILDER_DEFS = {
     'cargo': cargo.cargo,
