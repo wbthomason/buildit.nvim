251de15d16e0b79a332f80e0490b7f4cd56b6b1a
Wil Thomason <wil.thomason@gmail.com>
2017-11-29 23:20:16 -0500

    Add jbuilder support

--- rplugin/python3/buildit/builders/__init__.py
+++ rplugin/python3/buildit/builders/__init__.py
@@ -1,16 +1,12 @@
 ''' Standard builder definitions for BuildIt '''
 
-from . import cargo
-from . import cmake
-from . import gobuild
-from . import make
-from . import oasis
-from . import stack
+from . import cargo, cmake, gobuild, jbuilder, make, oasis, stack
 
 BUILDER_DEFS = {
     'cargo': cargo.cargo,
     'cmake': cmake.cmake,
     'go build': gobuild.go_build,
+    'jbuilder': jbuilder.jbuilder,
     'make': make.make,
     'oasis': oasis.oasis,
     'stack': stack.stack
---
+++ rplugin/python3/buildit/builders/jbuilder.py
@@ -0,0 +1,11 @@
+''' Builder definitions for OCaml jbuilder '''
+
+
+jbuilder = {
+    'sig': r'jbuild|_build',
+    'cmd': 'jbuilder build',
+    'func': None,
+    'ft': 'ocaml',
+    'subdir': None,
+    'shell': False
+}
