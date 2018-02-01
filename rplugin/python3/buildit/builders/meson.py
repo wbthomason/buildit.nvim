5d785b9c6e56d0ead2fd5b31840b726a0d4a4d1b
Wil Thomason <wil.thomason@gmail.com>
2018-02-01 15:35:20 -0500

    Ensure meson build is run from the correct directory

--- rplugin/python3/buildit/builders/meson.py
+++ rplugin/python3/buildit/builders/meson.py
@@ -6,10 +6,14 @@ from subprocess import check_call, CalledProcessError
 def meson_init(meson_dir):
   '''Initialize meson if it hasn't already been (if the build dir isn't there)'''
   if not os.path.isdir(os.path.join(meson_dir, 'build')):
+    curr_dir = os.path.curdir
     try:
+      os.chdir(meson_dir)
       check_call(['meson', 'build'])
     except CalledProcessError:
       return False
+    finally:
+      os.chdir(curr_dir)
 
   return True
 
