8f58cd003fb6657c76f28ac524d437561d2288ec
Wil Thomason <wil.thomason@gmail.com>
2018-02-01 15:25:23 -0500

    Meson build needs to be split into args

--- rplugin/python3/buildit/builders/meson.py
+++ rplugin/python3/buildit/builders/meson.py
@@ -7,7 +7,7 @@ def meson_init(meson_dir):
   '''Initialize meson if it hasn't already been (if the build dir isn't there)'''
   if not os.path.isdir(os.path.join(meson_dir, 'build')):
     try:
-      check_call("meson build")
+      check_call(['meson', 'build'])
     except CalledProcessError:
       return False
 
